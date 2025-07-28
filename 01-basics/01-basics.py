58c03ee692f805651071a831971dd8d5c08b67ac
Sathya <neelasathya06@gmail.com>
2025-07-28 07:42:50 -0400

    python identify classtypes and variable declarations

---
+++ 01-basics/01-basics.py
@@ -0,0 +1,15 @@
+# print('Hello python!')
+
+# Identify Classtypes
+x = "vino"
+# print(x.__class__.__name__)
+# print(type(x))
+
+# variables
+my_var = 10
+print(my_var)
+
+# java
+# Static vs Dynamic programming
+# x1 = 4
+# x = "sathya";
