ce767172513b389eab8312489a363da3d0a1115e
Sathya <neelasathya06@gmail.com>
2025-07-29 23:40:49 -0400

    binary search impl using while loop

---
+++ 05-Searching/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(list, target):
+    left = 0
+    right = len(list) - 1
+    while left < right:
+        middle = (left + right) // 2  # // - integer division
+        if target == list[middle]:
+            return middle
+        elif target < list[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
+
+list = [1,2,3,4,5,6,7,8]
+target = 6
+print(binary_search(list, target))
