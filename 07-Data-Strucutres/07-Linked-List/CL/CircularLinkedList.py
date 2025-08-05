1aefdd0ebe7132a9f0394ef291268f4f1d1a3de1
Sathya <neelasathya06@gmail.com>
2025-08-05 11:31:22 -0400

    CircularLinkedList addFirst and addLast impl

---
+++ 07-Data-Strucutres/07-Linked-List/CL/CircularLinkedList.py
@@ -0,0 +1,63 @@
+class Node:
+    __slots__ = ('_element', '_next')
+
+    def __init__(self, element, next):
+        self._element = element
+        self._next = next
+
+class CircularLinkedList:
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def isempty(self):
+        return self._size == 0
+
+    def display(self):
+        p = self._head
+        i = 0
+        while i < len(self):
+            print(p._element, end='-->')
+            p = p._next
+            i += 1
+        print()
+
+    def addFirst(self, e):
+        newest = Node(e, None)
+        if self.isempty():
+            newest._next = newest
+            self._head = newest
+            self._tail = newest
+        else:
+            self._tail._next = newest
+            newest._next = self._head
+        self._head = newest
+        self._size += 1
+
+
+    def addlast(self, e):
+        newest = Node(e, None)
+        if self.isempty():
+            newest._next = newest
+            self._head = newest
+            self._tail = newest
+        else:
+            newest._next = self._tail._next
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+
+C = CircularLinkedList()
+C.addlast(10)
+C.addlast(20)
+C.addlast(30)
+C.addlast(40)
+C.addFirst(5)
+C.display()
+print('Size: ', len(C))
