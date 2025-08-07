43d6eb852ad59c45f891974b868337226394090a
Sathya <neelasathya06@gmail.com>
2025-08-07 14:28:14 -0400

    all order traversals except inorder impl

--- 07-Data-Strucutres/10-Trees/00-Binary-Tree.py
+++ 07-Data-Strucutres/10-Trees/00-Binary-Tree.py
@@ -16,12 +16,12 @@ class BinaryTree:
     def inOrder(self, root):
         if root:
             self.inOrder(root._left)
-            print(root._element, end = '')
+            print(root._element, end = '->')
             self.inOrder(root._right)
 
     def preOrder(self, root):
         if root:
-            print(root._element,end='')
+            print(root._element,end='->')
             self.preOrder(root._left)
             self.preOrder(root._right)
 
@@ -29,8 +29,24 @@ class BinaryTree:
         if root:
             self.postOrder(root._left)
             self.postOrder(root._right)
-            print(root._element, end = ' ')
+            print(root._element, end = '->')
 
     def levelOrder(self, root):
         if root:
             pass
+
+X = BinaryTree()
+Y = BinaryTree()
+Z = BinaryTree()
+a = BinaryTree()
+X.maketree(20, a, a)
+Y.maketree(30, a, a)
+Z.maketree(10, X, Y)
+print('In Order Traversal: ')
+Z.inOrder(Z._root)
+print('\n')
+print('Pre Order Traversal: ')
+Z.preOrder(Z._root)
+print('\n')
+print('Post Order Traversal: ')
+Z.postOrder(Z._root)
