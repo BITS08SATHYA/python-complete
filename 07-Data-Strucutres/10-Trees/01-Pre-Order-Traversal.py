7f69dedafc4c1b53e35cfab4e9c9464898086499
Sathya <neelasathya06@gmail.com>
2025-08-07 14:09:50 -0400

    01- Pre-Order Impl

---
+++ 07-Data-Strucutres/10-Trees/01-Pre-Order-Traversal.py
@@ -0,0 +1,10 @@
+# Root -> Left -> Right
+# Visit Root
+# Visit Left Subtree Recursively PreOrder
+# Visit Right Subtree Recursively PreOrder
+
+#           A
+#      B         C
+#   D     E   F     G
+
+# Pre order Traversal : A, B, D, E, C, F, G
