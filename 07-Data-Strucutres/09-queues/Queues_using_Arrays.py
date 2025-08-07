d0bef085e4700455afab2c3fec149a6414bed2d4
Sathya <neelasathya06@gmail.com>
2025-08-06 23:29:26 -0400

    Queue using Arrays inbuilt python list

---
+++ 07-Data-Strucutres/09-queues/Queues_using_Arrays.py
@@ -0,0 +1,34 @@
+class QueuesArrays:
+    def __init__(self):
+        self._data = []
+        self._size = 0
+
+    def __len__(self):
+        return len(self._data)
+
+    def isempty(self):
+        return self._size == 0
+
+    def enqueue(self, e):
+        self._data.append(e)
+        self._size += 1
+
+    def dequeue(self):
+        if self.isempty():
+            print('Queue is empty!')
+            return
+        return self._data.pop(0)
+
+    def first(self):
+        if self.isempty():
+            print('Queue is empty!')
+            return
+        return self._data[0]
+
+Q = QueuesArrays()
+Q.enqueue(1)
+Q.enqueue(2)
+Q.enqueue(3)
+print(Q._data)
+Q.dequeue()
+print(Q._data)
