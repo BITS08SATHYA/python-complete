691f8d802a188feaa3dc297c6af7fd0f98a90c15
Sathya <neelasathya06@gmail.com>
2025-08-07 14:10:09 -0400

    01- Post-Order Impl

---
+++ 07-Data-Strucutres/10-Trees/03-Post-Order-Traversal.py
@@ -0,0 +1,10 @@
+# Root -> Left -> Right
+# Visit Left Subtree Recursively PreOrder
+# Visit Right Subtree Recursively PreOrder
+# Visit Root
+
+#           A
+#      B         C
+#   D     E   F     G
+
+# Post order Traversal : D, E, B, F, G, C, A
