d2cb6ba555ebb5bf77aa4c28ed8c46fffc06ec9e
Sathya <neelasathya06@gmail.com>
2025-08-07 14:10:21 -0400

    04- level-Order Impl

---
+++ 07-Data-Strucutres/10-Trees/04-Level-Order-Traversal.py
@@ -0,0 +1,9 @@
+# Root -> Left -> Right
+# Visit nodes Level by Level from top to bottom
+# within level visit nodes from left to right
+
+#           A           <-- Level 1
+#      B         C      <-- Level 2
+#   D     E   F     G   <-- Level 3
+
+# Level order Traversal : A, B, C, D, E, F, G
