d2b8781eba879324cddcc31f6b4ea5838e5bcdc5
Sathya <neelasathya06@gmail.com>
2025-08-11 17:35:52 -0400

    Working out the linked list, circular Linked and doubly linked list implemented

--- 07_Data_Strucutres/07-List/Exs/CLL/CLL_1.py
+++ 07_Data_Strucutres/07-List/Exs/CLL/CLL_1.py
@@ -48,3 +48,48 @@ class CircularLinkedList:
         self._tail._next = newest
         self._size += 1
 
+    def addAny(self, element, pos):
+        newest = Node(element, None)
+        if self.isempty():
+            print("List is empty")
+            return
+        elif pos == 0:
+            self.addFirst(element)
+        elif pos == self._size - 1:
+            self.addLast(element)
+        else:
+            p = self._head
+            i = 1
+            while i < pos - 1:
+                p = p._next
+                i += 1
+            newest._next = p._next
+            p._next = newest
+        self._size += 1
+
+    def removeFirst(self):
+        if self.isempty():
+            print("List is empty")
+            return
+        self._tail._next = self._head._next
+        self._head = self._head._next
+        self._size -= 1
+        if self.isempty():
+            self._head = None
+            self._tail = None
+
+    def removeLast(self):
+        if self.isempty():
+            print("List is empty")
+            return
+        p = self._head
+        i = 1
+        while p and i < self._size - 1:
+            p = p._next
+            i += 1
+        p._next = self._head
+        self._tail = p
+        self._size -= 1
+        if self.isempty():
+            self._head = None
+            self._tail = None
