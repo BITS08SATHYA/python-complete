494c13b83f327bf72799070cef3c2196c294ce53
Sathya <neelasathya06@gmail.com>
2025-08-16 13:11:48 -0400

    BST delete method using iterative impl

--- 07_Data_Strucutres/11_Binary_Trees/Exs/BST.py
+++ 07_Data_Strucutres/11_Binary_Trees/Exs/BST.py
@@ -1,3 +1,5 @@
+from graphviz import Digraph
+
 class _Node:
     __slots__ = '_element', '_left', '_right'
 
@@ -28,6 +30,17 @@ class BinarySearchTree:
             print(troot._element, end='->')
             self.inorder(troot._right)
 
+    def preOrder(self, troot):
+        if troot:
+            print(troot._element, end='->')
+            self.preOrder(troot._left)
+            self.preOrder(troot._right)
+
+    def postOrder(self, troot):
+        if troot:
+            self.postOrder(troot._left)
+            self.postOrder(troot._right)
+            print(troot._element, end='->')
 
 
     def rsearch(self, troot, key):
@@ -74,16 +87,97 @@ class BinarySearchTree:
             troot = n
         return troot
 
+    def delete(self, e):
+        p = self._root
+        pp = None
+        # determine where the node to be deleted is
+        while p and p._element != e:
+            pp = p
+            if e < p._element:
+                p = p._left
+            else:
+                p = p._right
+        if not p:
+            return False
+        # deleting a node with two children
+        if p._left and p._right:
+            s = p._left
+            ps = p
+            while s._right:
+                ps = s
+                s = s._right
+            p._element = s._element
+            p = s
+            pp = ps # Now p has at most one child
+        # deleting a node with one child or no children
+        c = None
+        if p._left:
+            c = p._left
+        else:
+            c = p._right
+        # deleting a root node
+        if p == self._root:
+            self._root = c
+        else:
+            if p == pp._left:
+                pp._left = c
+            else:
+                pp._right = c
 
-B = BinarySearchTree()
-# Iterative
-B.insert(B._root, 50)
-B.insert(B._root, 30)
-B.insert(B._root, 80)
-B.insert(B._root, 10)
-B.insert(B._root, 40)
-B.insert(B._root, 60)
-B.inorder(B._root)
 
+class BinarySearchTreeVisualizer(BinarySearchTree):
+    def visualize(self, troot):
+        def add_nodes_edges(dot, node):
+            if node:
+                dot.node(str(id(node)), str(node._element))
+                if node._left:
+                    dot.edge(str(id(node)), str(id(node._left)))
+                    add_nodes_edges(dot, node._left)
+                if node._right:
+                    dot.edge(str(id(node)), str(id(node._right)))
+                    add_nodes_edges(dot, node._right)
+
+        dot = Digraph()
+        add_nodes_edges(dot, troot)
+        return dot
+
+
+# B = BinarySearchTree()
+# # Iterative
+# # B.insert(B._root, 50)
+# # B.insert(B._root, 30)
+# # B.insert(B._root, 80)
+# # B.insert(B._root, 10)
+# # B.insert(B._root, 40)
+# # B.insert(B._root, 60)
+#
+# B._root = B.rinsert(B._root, 50)
+# B.rinsert(B._root, 30)
+# B.rinsert(B._root, 80)
+# B.rinsert(B._root, 10)
+# B.inorder(B._root)
+# print('\n')
+# B.preOrder(B._root)
+# print('\n')
+# B.delete(30)
+# B.inorder(B._root)
+
+# Example usage
+B = BinarySearchTreeVisualizer()
+B._root = B.rinsert(B._root, 50)
+B.rinsert(B._root, 30)
+B.rinsert(B._root, 80)
+B.rinsert(B._root, 10)
+
+# Visualize before deletion
+dot = B.visualize(B._root)
+dot.render('tree_before', format='png', cleanup=True)
+
+# Perform deletion
+B.delete(30)
+
+# Visualize after deletion
+dot = B.visualize(B._root)
+dot.render('tree_after', format='png', cleanup=True)
 
 
