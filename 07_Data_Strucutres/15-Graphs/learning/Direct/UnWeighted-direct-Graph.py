428fb9275a3053d24024defa9aa3a1d139a6d2ba
Sathya <neelasathya06@gmail.com>
2025-08-22 10:58:03 -0400

    BFS impl

--- 07_Data_Strucutres/15-Graphs/learning/Direct/UnWeighted-direct-Graph.py
+++ 07_Data_Strucutres/15-Graphs/learning/Direct/UnWeighted-direct-Graph.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Queues_LL import QueuesLinkedList
 
 class Graph:
     def __init__(self, vertices):
@@ -53,18 +54,57 @@ class Graph:
     def display_adjMat(self):
         print(self._adjMat)
 
-G = Graph(4)
-G.display_adjMat()
-print('Vertices: ', G.vertex_count())
-# print('Edges: ', G.edge_count())
+    def BFS(self, s):
+        i = s
+        q = QueuesLinkedList()
+        visited = [0] * self._vertices
+        print(i, end = ' ')
+        visited[i] = 1
+        q.enqueue(i)
+        while not q.is_empty():
+            i = q.dequeue()
+            for j in range(self._vertices):
+                if self._adjMat[i][j] == 1 and visited[j] == 0:
+                    print(j, end = ' ')
+                    visited[j] = 1
+                    q.enqueue(j)
+
+
+G = Graph(7)
 G.insert_edge(0, 1)
-G.insert_edge(0, 2)
+G.insert_edge(0, 5)
+G.insert_edge(0, 6)
+G.insert_edge(1, 0)
 G.insert_edge(1, 2)
+G.insert_edge(1, 5)
+G.insert_edge(1, 6)
 G.insert_edge(2, 3)
-G.display_adjMat()
-print('Vertices: ', G.vertex_count())
-print('Edges: ', G.edge_count())
-G.edges()
-G.exist_edge(1, 3)
-print('Edge between 1 and 3: ', G.exist_edge(1, 3))
-print('Edge between 2 and 3: ', G.exist_edge(2, 3))
+G.insert_edge(2, 4)
+G.insert_edge(2, 6)
+G.insert_edge(3, 4)
+G.insert_edge(4, 2)
+G.insert_edge(4, 5)
+G.insert_edge(5, 2)
+G.insert_edge(5, 3)
+G.insert_edge(6, 3)
+print('BFS')
+G.BFS(0)
+print("\nBFS from vertex 4")
+G.BFS(4)
+
+
+# G = Graph(4)
+# G.display_adjMat()
+# print('Vertices: ', G.vertex_count())
+# # print('Edges: ', G.edge_count())
+# G.insert_edge(0, 1)
+# G.insert_edge(0, 2)
+# G.insert_edge(1, 2)
+# G.insert_edge(2, 3)
+# G.display_adjMat()
+# print('Vertices: ', G.vertex_count())
+# print('Edges: ', G.edge_count())
+# G.edges()
+# G.exist_edge(1, 3)
+# print('Edge between 1 and 3: ', G.exist_edge(1, 3))
+# print('Edge between 2 and 3: ', G.exist_edge(2, 3))
