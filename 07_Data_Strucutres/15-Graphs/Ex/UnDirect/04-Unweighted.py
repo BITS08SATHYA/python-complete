15aa3b4b2bd83656cd9b9f852ad7f78a30bd33fb
Sathya <neelasathya06@gmail.com>
2025-08-26 19:22:16 -0400

    UnWeighted

--- 07_Data_Strucutres/15-Graphs/Ex/UnDirect/04-Unweighted.py
+++ 07_Data_Strucutres/15-Graphs/Ex/UnDirect/04-Unweighted.py
@@ -1,9 +1,11 @@
 import numpy as np
+from Queues_LL import QueuesLinkedList
 
 class Graph:
     def __init__(self, vertices):
         self._vertices = vertices
         self._adjMat = np.zeros((vertices, vertices))
+        self._visited = [0] * vertices
 
     def insertEdge(self, u , v, x = 1):
         self._adjMat[u][v] = x
@@ -53,6 +55,30 @@ class Graph:
     def display_adjMat(self):
         print(self._adjMat)
 
+    def BFS(self, s):
+        i = s
+        q = QueuesLinkedList()
+        visited = [0] * self._vertices
+        print(i, end= ' ')
+        visited[i] = 1
+        q.enqueue(i)
+        while not q.is_empty():
+            i = q.dequeue()
+            for j in range(self._vertices):
+                if self._adjMat[i][j] == 1 and visited[j] == 0:
+                    print(j, end = ' ')
+                    visited[j] = 1
+                    q.enqueue(j)
+
+    def DFS(self, s):
+        if self._visited[s] == 0:
+            print(s, end = ' ')
+            self._visited[s] = 1
+            for j in range(self._vertices):
+                if self._adjMat[s][j] == 1 and self._visited[j] == 0:
+                    self.DFS(j)
+
+
 # UnDirected Unweighted graphs
 G = Graph(4)
 G.display_adjMat()
@@ -66,14 +92,15 @@ G.insertEdge(2,0)
 G.insertEdge(2,1)
 G.insertEdge(2,3)
 G.insertEdge(3,2)
-G.display_adjMat()
-print('Edges: ', G.edge_count())
-G.edges()
-print('Edge between 1-2',G.exist_edge(1,2))
-print('Degree', G.indegree(2))
-G.removeEdge(1,2)
-print('Edge between 1-2', G.exist_edge(1,2))
+# G.display_adjMat()
+# print('Edges: ', G.edge_count())
+# G.edges()
+# print('Edge between 1-2',G.exist_edge(1,2))
+# print('Degree', G.indegree(2))
+# G.removeEdge(1,2)
+# print('Edge between 1-2', G.exist_edge(1,2))
 
-
+# G.BFS(0)
+G.DFS(2)
 
 
