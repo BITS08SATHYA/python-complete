918120c4c31793c3d649d0676129b8f840ac479d
Sathya <neelasathya06@gmail.com>
2025-08-25 22:04:43 -0400

    directed unweighted graphs impl

---
+++ 07_Data_Strucutres/15-Graphs/Ex/Direct/02-UnWeighted.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+class Graph:
+    def __init__(self, vertices):
+        self._vertices = vertices
+        self._adjMat = np.zeros((vertices, vertices))
+
+    def insertEdge(self, u , v, x = 1):
+        self._adjMat[u][v] = x
+
+    def removeEdge(self, u, v):
+        self._adjMat[u][v] = 0
+
+    def exist_edge(self, u, v):
+        return self._adjMat[u][v] != 0
+
+    def vertex_count(self):
+        return self._vertices
+
+    def edge_count(self):
+        count = 0
+        for i in range(self._vertices):
+            for j in range(self._vertices):
+                if self._adjMat[i][j] != 0:
+                    count += 1
+        return count
+
+    def vertices(self):
+        for i in range(self._vertices):
+            print(i, end=' ')
+        print()
+
+    def edges(self):
+        for i in range(self._vertices):
+            for j in range(self._vertices):
+                if self._adjMat[i][j] != 0:
+                    print(i, '--', j)
+
+    def outdegree(self, v):
+        count = 0
+        for j in range(self._vertices):
+            if self._adjMat[v][j] != 0:
+                count += 1
+        return count
+
+    def indegree(self, v):
+        count = 0
+        for j in range(self._vertices):
+            if self._adjMat[j][v] != 0:
+                count += 1
+        return count
+
+    def display_adjMat(self):
+        print(self._adjMat)
+
+# Directed Unweighted graphs
+G = Graph(4)
+G.display_adjMat()
+print( 'Vertices: ' ,G.vertex_count())
+print( 'Edges: ' ,G.edge_count())
+G.insertEdge(0, 1)
+G.insertEdge(0, 2)
+G.insertEdge(1,2)
+G.insertEdge(2,3)
+G.display_adjMat()
+print('Edges: ', G.edge_count())
+G.edges()
+print('Edge between 1-3',G.exist_edge(1,3))
+print('Degree', G.indegree(2))
+print('Edge between 1-2', G.exist_edge(1,2))
+G.removeEdge(1,2)
+print('Edge between 1-2', G.exist_edge(1,2))
+print('Outdegree 2:', G.outdegree(2))
+
+
+
